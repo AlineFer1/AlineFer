7c84b1b9e4ee29e97a7949b7273a3933a1ac1d4a
AlineFer1 <alinesouzaferreira@hotmail.com>
2025-03-23 23:26:16 -0400

    Update emailToSMSConfig.py
    details

--- emailToSMSConfig.py
+++ emailToSMSConfig.py
@@ -1,8 +1,10 @@
-   
-senderEmail = ['alinesf016@gmail.com']
-    
-gatewayAddress = ['3213338757@voicestream.net']
-    
-appkey = ['tkblzrdthliwomwo']
+# -*- coding: utf-8 -*-
+"""
+Email to SMS Gateway - config
+@author: adam getbags
+"""
 
+senderEmail = 'email@address.com'
+gatewayAddress = '1112224444@vmobl.com'
+appKey = 'abcd efgh ijkl mnop'
 
